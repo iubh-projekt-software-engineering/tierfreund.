38912a635a1051b16117ca4d82d7c65fc154d9fc
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-06-06 20:50:57 +0200

    Templating & styling

--- theme/js/index.js
+++ theme/js/index.js
@@ -1,1 +1,25 @@
-console.log('TEST');
+import Stepmation from './stepmation';
+
+const myStepmation = new Stepmation(document.querySelector('.onboarding--stage'));
+
+document.querySelector('.onboarding--stage_next').addEventListener('click', () => {
+    myStepmation.next();
+});
+
+myStepmation.subscribe('in.before', (instance, container) => {    
+    container.style.display = 'block';
+});
+myStepmation.subscribe('out.after', (instance, container) => {    
+    if (instance.index < 1) {
+        return;
+    }
+    instance.items[(instance.index - 1)].style.display = 'none';
+    [].slice.call(document.querySelectorAll('.onboarding--stage_step')).forEach((el, index) => {
+        if (instance.index === index) {
+            el.classList.add('is--active');
+        } else {
+            el.classList.remove('is--active');
+        }
+    });
+});
+myStepmation.init();
