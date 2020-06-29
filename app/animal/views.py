992512c1054dddcb131eed1a8ec07007264cfeac
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-06-29 19:53:33 +0200

    Add flying button for create url

--- app/animal/views.py
+++ app/animal/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, url_for
 from flask_login import current_user
 from app import db
 from app.animal.model import Animal
@@ -10,4 +10,13 @@ mod = Blueprint('animal', __name__, url_prefix='/tiere')
 @mod.route('/')
 def index():
     animals = db.session.query(Animal).filter_by(user_id=current_user.id).all()
-    return render_template('/animals/index.html', animals=animals)
+    create_url = url_for('animal.create')
+    return render_template(
+        '/animals/index.html',
+        animals=animals,
+        create_url=create_url
+    )
+
+@mod.route('/erstellen')
+def create():
+    return render_template('/animals/create.html')
