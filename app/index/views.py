21e1344fddb0e4f610743f157c5caca3256e940f
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-07-09 22:03:05 +0200

    Update views.py

--- app/index/views.py
+++ app/index/views.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
 
 mod = Blueprint('index', __name__)
 
 
 @mod.route('/')
 def index():
+    if hasattr(current_user, 'id'):
+        return redirect(url_for('dashboard.index'))
     return render_template('/home/index.html')
