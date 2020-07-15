03b8a5e2e02c6d709dfc2d9523f8474df25f6fe3
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2020-07-15 17:41:51 +0200

    Added Event class attributes

--- app/event/model.py
+++ app/event/model.py
@@ -7,6 +7,11 @@ class Event(db.Model):
     __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    titel = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    topic = db.Column(db.String(100))
+    notes = db.Column(db.String(100))
+    reminder = db.Column(db.Integer, nullable=False)
     animal_id = db.Column(db.Integer, db.ForeignKey('animal.id'))
     animal = db.relationship(Animal, foreign_keys=[animal_id])
     doc_id = db.Column(db.Integer, db.ForeignKey('doc.id'))
