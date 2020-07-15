3f05a47e94d67c8fa293b74de8bb6102514b636f
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2020-07-15 17:36:39 +0200

    Replaced user with animal

--- app/event/model.py
+++ app/event/model.py
@@ -1,5 +1,5 @@
 from app import db
-from app.user.models import User
+from app.user.models import Animal
 from app.doc.models import Doc
 
 
@@ -7,7 +7,7 @@ class Event(db.Model):
     __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship(User, foreign_keys=[user_id])
+    animal_id = db.Column(db.Integer, db.ForeignKey('animal.id'))
+    animal = db.relationship(Animal, foreign_keys=[animal_id])
     doc_id = db.Column(db.Integer, db.ForeignKey('doc.id'))
     doc = db.relationship(Doc, foreign_keys[doc_id])
