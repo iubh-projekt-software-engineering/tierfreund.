9583362ba3d802eacfae965fa088fad4dc372f62
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2020-07-15 17:14:34 +0200

    Added user relationship to Event class

--- app/event/model.py
+++ app/event/model.py
@@ -4,3 +4,6 @@ from app.user.models import User
 
 class Event(db.Model):
     __tablename__ = 'events'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User, foreign_keys=[user_id])
