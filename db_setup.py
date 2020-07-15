83280b53bf5f01f8a2bf56184908a38ea3bb2996
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2020-07-15 17:10:12 +0200

    Added Event class to db_setup

--- db_setup.py
+++ db_setup.py
@@ -4,6 +4,7 @@ from app import db
 from app.user.models import User
 from app.animal.model import Animal
 from app.doc.model import Doc
+from app.event.model import Event
 
 if __name__ == '__main__':
     db.create_all()
