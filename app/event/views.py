eaa3a55e0b53b66d8326a65ac55b08ae243d299c
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-07-17 22:09:53 +0200

    Create event

--- app/event/views.py
+++ app/event/views.py
@@ -29,8 +29,8 @@ def create():
     if request.method == 'POST':
         try:
             new_event = Event(
-                animal_id=Animal.id,
-                doc_id=Doc.id,
+                animal_id=request.form.get('animal_id'),
+                doc_id=request.form.get('doc_id'),
                 titel=request.form.get('titel'),
                 time=request.form.get('time'),
                 topic=request.form.get('topic'),
