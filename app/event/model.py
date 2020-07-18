8e74e7bedb5b94f612d398b506dc63c1cab0df15
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-07-18 20:10:35 +0200

    Add required model fields in events

--- app/event/model.py
+++ app/event/model.py
@@ -8,9 +8,9 @@ class Event(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     time = db.Column(db.DateTime, nullable=False)
-    topic = db.Column(db.String(100))
+    topic = db.Column(db.String(100), nullable=False)
     notes = db.Column(db.String(200))
-    animal_id = db.Column(db.Integer, db.ForeignKey('animals.id'))
+    animal_id = db.Column(db.Integer, db.ForeignKey('animals.id'), nullable=False)
     animal = db.relationship(Animal, foreign_keys=[animal_id])
-    doc_id = db.Column(db.Integer, db.ForeignKey('docs.id'))
+    doc_id = db.Column(db.Integer, db.ForeignKey('docs.id'), nullable=False)
     doc = db.relationship(Doc, foreign_keys=[doc_id])
