9ec9f8d25787736ddc38b8560b00e7e8a9996536
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2020-07-19 21:29:03 +0200

    privacy linted

--- app/privacy/views.py
+++ app/privacy/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import current_user
+from flask import Blueprint, render_template
+
 
 mod = Blueprint('privacy', __name__, url_prefix='/datenschutz')
 
